c8c6cac5728b166878a84cdfda2fa8caeaba12bf
Dmytro Perepelytsa <umniyd@gmail.com>
2025-01-24 22:26:00 +0200

    Feat: basic setup, added eslint, ngrx, tailwind, cdk

---
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'tp-welcome-page',
+  templateUrl: './welcome-page.component.html',
+})
+export class WelcomePageComponent {}
